8873edbfb0a24c1e024a6e5072a43fa5ad54421c
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-05-15 20:39:21 +0530

    day1 update

---
+++ Dsa/VaibhavTalkhande/Day1/day1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a='a';
+    cout<<a<<endl;
+    char b ='v';
+    cout << b << endl;
+    bool bl=true;
+    cout << bl <<endl;
+    float fl=1.2;
+    cout << fl <<endl;
+    double d=1.223;
+    cout<<d<<endl;
+    cout<< sizeof(a) <<endl;
+
+    return 0;
+}
