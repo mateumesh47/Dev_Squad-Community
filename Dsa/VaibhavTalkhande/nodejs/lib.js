b53b04d9c1f7a26d050a270d9830339bbe9aafc3
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-06-02 19:39:31 +0530

    node js

---
+++ Dsa/VaibhavTalkhande/nodejs/lib.js
@@ -0,0 +1,16 @@
+function sum(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mul(a,b){
+    return a*b;
+
+}
+
+exports.sum = sum;
+exports.sub = sub;
+exports.mul = mul;
