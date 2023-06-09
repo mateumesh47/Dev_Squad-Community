3b6626569e7d1f49c3a6875c3ef1cb8543808a61
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-06-09 16:23:12 +0530

    testing sync performance

--- Dsa/VaibhavTalkhande/nodejs/index.js
+++ Dsa/VaibhavTalkhande/nodejs/index.js
@@ -1,19 +1,27 @@
-const lib = require('./lib.js')
-const fs = require('fs');
-console.log(performance.now());
-// fs.readFile('Text.txt', 'utf-8', (err, data) => {
-//       if (err) {
-//             console.log(err);
-//       }
-//       else {
-//           console.log(data);
-//       }
-// });
-const data = fs.readFileSync('Text.txt', 'utf-8');
-console.log(data);
-console.log(performance.now());
-console.log(lib.sum(1, 2));
-console.log(lib.sub(1, 2));
-console.log(lib.mul(1, 2));
+// const lib = require('./lib.js')
+// const fs = require('fs');
+// console.log(performance.now());
+// // fs.readFile('Text.txt', 'utf-8', (err, data) => {
+// //       if (err) {
+// //             console.log(err);
+// //       }
+// //       else {
+// //           console.log(data);
+// //       }
+// // });
+// const data = fs.readFileSync('Text.txt', 'utf-8');
+// console.log(data);
+// console.log(performance.now());
+// console.log(lib.sum(1, 2));
+// console.log(lib.sub(1, 2));
+// console.log(lib.mul(1, 2));
 
-console.log(performance.now());
+// console.log(performance.now());
+const http = require('http');
+const server = http.createServer((req, res) => {
+     console.log('server created');
+       res.end('Hello from the server');
+      
+}
+);
+server.listen(3000);
